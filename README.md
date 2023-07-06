b6f6bdeda3f229d20a38a3be93c9d20863fa7d08
Ronan M <ronan.mccoy@mach49.com>
2023-07-06 00:09:00 -0700

    initial commit and completed code for the goal.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Languages From Meta Data
+
+Pull a unique list of languages from the array of options for the multiselect fields used for language proficiency. Store the resulting list to a .txt file.
+
+Reference: https://airtable.com/api/meta
+
+## To Do
+
+Replace API key with PAT.
+
---
+++ index.js
@@ -0,0 +1,66 @@
+require('dotenv').config({path: './.env'});
+const axios = require('axios').default;
+const fs = require('fs');
+
+const getMetadata = async (apiUrl, baseId) => {
+  const req = apiUrl + 'meta/bases/' + baseId + '/tables';
+
+  return await axios.get(req, {
+    headers: {'Authorization': 'Bearer ' + process.env.AIRTABLE_API_KEY}
+  }).catch((error) => {
+    throw new Error(error);
+  });
+};
+
+const main = async () => {
+  console.log('Starting...');
+
+  const airtableApi = process.env.AIRTABLE_API_URL + '/' + process.env.AIRTABLE_API_VERSION + '/';
+
+  let data = await getMetadata(airtableApi, process.env.AIRTABLE_BASE_ID);
+  let tables = data.data.tables;
+  let languages = [];
+
+  for(const table of tables) {
+    console.log(table.name);
+    if(table.name === process.env.AIRTABLE_BASE_TABLE_NAME) {
+      let fields = table.fields;
+
+      for(const field of fields) {
+        if(field.name === 'Native Speaker' 
+          || field.name === 'Business Fluent' 
+            || field.name === 'Language Learner'
+        ) {
+          let options = field.options.choices;
+
+          for(const option of options) {
+            let optionValue = option.name;
+
+            if(languages.indexOf(optionValue) === -1) {
+              languages.push(optionValue);
+            }
+          }
+        }
+      }
+      break;
+    }
+  }
+
+  if(languages.length > 0) {
+    languages.sort();
+
+    try {
+      fs.writeFileSync('./languages.txt', languages.join('\n'));
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  console.log(languages);
+};
+
+main().catch((reason) => {
+  console.error(reason);
+  console.error('Exiting...');
+  process.exit(1);
+});
